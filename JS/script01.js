eb2aaa187edabceee9959ff5c3cf52e0d240545a
Tommy Humble <vtomshin@naumen.ru>
2023-01-20 12:17:56 +0500

    Exercise 1

--- JS/script01.js
+++ JS/script01.js
@@ -1,3 +1,20 @@
 "use strictr";
 
-alert('Wake the fuck up, Samurai!');
+const numberOfFilms = +prompt("Сколлько фильмов вы уже смотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false};
+    
+const q1 = prompt("Один из последних просмотренных фиьмов?");
+const q2 = prompt("На сколько оцените его?");
+const q3 = prompt("Один из последних просмотренных фиьмов?");
+const q4 = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[q1] = q2;
+personalMovieDB.movies[q3] = q4;
+
+console.log(personalMovieDB);
